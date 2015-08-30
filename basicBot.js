d6baac16e5baefdddeacab14e7d9516ebd7506af
RobGoldy <nine5one@gmail.com>
2015-08-30 02:08:00 -0700

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -240,9 +240,9 @@
         status: false,
         name: "basicBot",
         loggedInID: null,
-        scriptLink: "https://rawgit.com/Yemasthui/basicBot/master/basicBot.js",
+        scriptLink: "https://rawgit.com/Upcoming-Records/basicBot/master/basicBot.js",
         cmdLink: "http://git.io/245Ppg",
-        chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
+        chatLink: "https://rawgit.com/Upcoming-Records/basicBot/master/lang/en.json",
         chat: null,
         loadChat: loadChat,
         retrieveSettings: retrieveSettings,
@@ -250,8 +250,8 @@
         settings: {
             botName: "basicBot",
             language: "english",
-            chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
-            scriptLink: "https://rawgit.com/Yemasthui/basicBot/master/basicBot.js",
+            chatLink: "https://rawgit.com/Upcoming-Records/basicBot/master/lang/en.json",
+            scriptLink: "https://rawgit.com/Upcoming-Records/basicBot/master/basicBot.js",
             roomLock: false, // Requires an extension to re-load the script
             startupCap: 1, // 1-200
             startupVolume: 0, // 0-100
