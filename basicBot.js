bb077f071fa9cb2aac506851bece153308e02bac
Al3366 <al3366odea@gmail.com>
2015-09-04 19:06:19 +0100

    Updated Links

--- basicBot.js
+++ basicBot.js
@@ -99,7 +99,7 @@
     var loadChat = function (cb) {
         if (!cb) cb = function () {
         };
-        $.get("https://rawgit.com/Yemasthui/basicBot/master/lang/langIndex.json", function (json) {
+        $.get("https://rawgit.com/Upcoming-Records/basicBot/master/lang/langIndex.json", function (json) {
             var link = basicBot.chatLink;
             if (json !== null && typeof json !== "undefined") {
                 langIndex = json;
