b4171a4e565f685157fbab6e9abc24ecd7024748
Al3366 <al3366odea@gmail.com>
2015-09-10 19:48:49 +0100

    More commands

--- basicBot.js
+++ basicBot.js
@@ -3579,6 +3579,32 @@
                 }
             },
 
+            updateCommand: {
+                command: 'update',
+                rank: 'bouncer',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        API.sendChat(basicBot.chat.update)
+                    }
+                }
+            },
+
+            slackCommand: {
+                command: 'slack',
+                rank: 'bouncer',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        API.sendChat(basicBot.chat.slack)
+                    }
+                }
+            },
+
             welcomeCommand: {
                 command: 'welcome',
                 rank: 'mod',
